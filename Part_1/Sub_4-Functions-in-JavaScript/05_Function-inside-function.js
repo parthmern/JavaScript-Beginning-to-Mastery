e7e4be1bebe9b41c6d6cc084ed9cdf1cb6a35e32
parthmern <125397720+parthmern@users.noreply.github.com>
2023-06-05 15:01:42 +0530

    Update 05_Function-inside-function.js

--- Part_1/Sub_4-Functions-in-JavaScript/05_Function-inside-function.js
+++ Part_1/Sub_4-Functions-in-JavaScript/05_Function-inside-function.js
@@ -6,11 +6,11 @@ const app = () => {  //main function
         console.log("hello from myFunc")
     }
 
-    const addTwo = (num1,num2) => {                  //sub function 1
+    const addTwo = (num1,num2) => {                  //sub function 2
         return num1 + num2 ;
     }
 
-    const multi = (num1,num2) => num1 * num2;        //sub function 1   
+    const multi = (num1,num2) => num1 * num2;        //sub function 3   
 
     console.log("inside app");                       //<-- this is for main function    
 
@@ -30,8 +30,8 @@ app();  //  <---- calling the MAIN (first-primary) function
 //         5
 //         9
 
+//console.log(addTwo(3,3)); <---- EROOR, we cannot call sub function outside the main function
 
 
-
-// step1))  first call the first function
+// step1))  first call the first/main function
 // step2))  secondly call the function under the main function like - myFunc() and addTwo(2,3) 
