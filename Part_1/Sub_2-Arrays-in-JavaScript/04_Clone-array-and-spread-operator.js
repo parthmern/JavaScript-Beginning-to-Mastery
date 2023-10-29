7effaa3bf2b051cc03aba01951874a56d165c46f
Parth Patel <125397720+parthmern@users.noreply.github.com>
2023-10-29 22:09:11 +0530

    Update 04_Clone-array-and-spread-operator.js

--- Part_1/Sub_2-Arrays-in-JavaScript/04_Clone-array-and-spread-operator.js
+++ Part_1/Sub_2-Arrays-in-JavaScript/04_Clone-array-and-spread-operator.js
@@ -17,3 +17,20 @@ console.log(array3);
 
 let extraArray = [...array1,...array2]; //['item1', 'item2','item1', 'item2']
 let oneArray = array1.slice(0).concat("item3","item4");  //['item1', 'item2','item3', 'item4']
+
+
+//========================================
+//========================================
+
+const fruits = ["apple", "banana", "cherry", "date", "elderberry"];
+
+const slicedFruits1 = fruits.slice(1, 3); // Extracts elements at index 1 and 2
+console.log(slicedFruits1); // ["banana", "cherry"]
+
+const slicedFruits2 = fruits.slice(2); // Extracts elements from index 2 to the end
+console.log(slicedFruits2); // ["cherry", "date", "elderberry"]
+
+const shallowCopy = fruits.slice(); // Creates a shallow copy of the entire array
+console.log(shallowCopy); // ["apple", "banana", "cherry", "date", "elderberry"]
+
+console.log(fruits); // Original array is not modified
