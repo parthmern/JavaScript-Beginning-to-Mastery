4996e9b0102b172a7f1078c98009e44edf3fb60b
Parth Patel <125397720+parthmern@users.noreply.github.com>
2024-01-07 18:11:16 -0500

    Update 02_This-keyword-and-Window-object.js

--- Part_1/Sub_7-Object-Oriented-JavaScript-- Prototypal-Inheritance/02_This-keyword-and-Window-object.js
+++ Part_1/Sub_7-Object-Oriented-JavaScript-- Prototypal-Inheritance/02_This-keyword-and-Window-object.js
@@ -20,6 +20,64 @@ myFunc(); // then ERROR
 
 
 
+// ===============================================================================
+// ===============================================================================
+
+// "this" KEYWORD
+
+// ---------------------------------
+// Global context
+console.log(this); // Refers to the global object (e.g., window in a browser)
+
+// ---------------------------------
+// Regular function
+function regularFunction() {
+  console.log(this);
+}
+regularFunction(); // Refers to the global object
+
+// ----------------------------------
+const person = {
+  name: "John",
+  sayHello: function () {
+    console.log(`Hello, my name is ${this.name}.`);
+  },
+  sayHelloArrow: () => {
+    console.log(`Hello, my name is ${this.name}.`);
+  },
+};
+
+person.sayHello(); // Output: Hello, my name is John.
+person.sayHelloArrow(); // Output: Hello, my name is undefined.
+
+
+// ----------------------------------
+// Object method
+const obj = {
+  name: 'Example',
+  greet() {
+    console.log(`Hello, ${this.name}!`);
+  }
+};
+obj.greet(); // Refers to the obj and prints "Hello, Example!"
+
+// ----------------------------------
+// Constructor function
+function Person(name) {
+  this.name = name;
+  this.sayName = function() {
+    console.log(`My name is ${this.name}.`);
+  };
+}
+const person1 = new Person('Alice');
+person1.sayName(); // Refers to person1 and prints "My name is Alice!"
+
+// -----------------------------------
+// Event handler
+const button = document.getElementById('myButton');
+button.addEventListener('click', function() {
+  console.log(this); // Refers to the DOM element 'myButton'
+});
 
 
 
